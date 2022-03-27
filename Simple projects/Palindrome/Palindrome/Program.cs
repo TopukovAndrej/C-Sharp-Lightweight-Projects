9014b2d19b123e9e19411562acd30b371696b938
Andrej Topukov <andrej.topukov.14@gmail.com>
2022-03-27 12:31:25 +0200

    Added Palindrome project

---
+++ Simple projects/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Insert a string to check if it's a palindrome!\n");
+            
+            string? str = Console.ReadLine();
+            Console.WriteLine("\n");
+
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                Console.WriteLine("String cannot be empty or contain only whitespaces.");
+                Console.ReadKey();
+            }
+            else
+            {
+                if (CheckIfPalindrome(str))
+                {
+                    Console.WriteLine(String.Format("The string {0} is a palindrome!\n", str));
+                }
+                else
+                {
+                    Console.WriteLine(String.Format("The string {0} is not a palindrome!\n", str));
+                }
+                Environment.Exit(0);
+            }
+        }
+
+        /// <summary>
+        /// Checks if the given string is a palindrome.
+        /// </summary>
+        /// <param name="s">The string that needs to be checked.</param>
+        /// <returns>true if the string is a palindrome, false otherwise.</returns>
+        static bool CheckIfPalindrome(string s)
+        {
+            int i = 0;
+            int j = s.Length - 1;
+
+            while (i != s.Length - 1 && j != 0)
+            {
+                if (s[i] != s[j])
+                {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+    }
+}
