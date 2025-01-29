40f91405a272828c35fd69b492cc818f91a97d1b
Andrej Topukov <88235830+TopukovAndrej@users.noreply.github.com>
2025-01-29 15:45:42 +0100

    Added FizzBuzz solution

---
+++ Simple projects/FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            bool isMultipleOfThree;
+            bool isMultipleOfFive;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                isMultipleOfThree = i % 3 == 0;
+                isMultipleOfFive = i % 5 == 0;
+
+                if (isMultipleOfThree && isMultipleOfFive)
+                {
+                    Console.WriteLine(value: "FizzBuzz");
+                }
+                else if (isMultipleOfThree)
+                {
+                    Console.WriteLine(value: "Fizz");
+                }
+                else if (isMultipleOfFive)
+                {
+                    Console.WriteLine(value: "Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(value: i);
+                }
+            }
+        }
+    }
+}
